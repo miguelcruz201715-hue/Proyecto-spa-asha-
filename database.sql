99c867632b6a45d7b6c5fd8cd7633bd63f3b351b
miguelcruz2017 <miguelcruz201715@gmail.com>
2025-11-15 11:41:49 -0500

    creacion factura con datos

--- database.sql
+++ database.sql
@@ -8,11 +8,19 @@ CREATE TABLE t_ciudad(
     CONSTRAINT      pk_ciudad PRIMARY KEY(id_ciudad)
 )ENGINE=InnoDb;
 
+INSERT INTO t_ciudad(id_ciudad,ciudad) VALUES (NULL, "Bucaramanga");
+INSERT INTO t_ciudad(id_ciudad,ciudad) VALUES (NULL, "Medellin");
+INSERT INTO t_ciudad(id_ciudad,ciudad) VALUES (NULL, "Cali");
+
 CREATE TABLE t_unidad(
     id_unidad       int(11)     AUTO_INCREMENT NOT NULL,
-    t_unidad        varchar(50),
+    unidad        varchar(50),
     CONSTRAINT      pk_unidad   PRIMARY KEY(id_unidad)
 )ENGINE=InnoDb;
+
+INSERT INTO t_unidad(id_unidad,unidad) VALUES(NULL,"UNIDAD");
+INSERT INTO t_unidad(id_unidad,unidad) VALUES(NULL,"KILO");
+INSERT INTO t_unidad(id_unidad,unidad) VALUES(NULL,"RESMA");
 
 CREATE TABLE t_cliente(
     id_cliente      int(11)     AUTO_INCREMENT NOT NULL,
@@ -31,5 +39,19 @@ CREATE TABLE t_producto(
     valor_unitario  float(10,2),
     CONSTRAINT      pk_producto  PRIMARY KEY(id_producto),
     CONSTRAINT      fr_unidad   FOREIGN  KEY(id_unidad) REFERENCES t_unidad(id_unidad)
+
+)ENGINE=InnoDb; 
+
+CREATE TABLE t_factura(
+    id_factura      int(11)        AUTO_INCREMENT NOT NULL,
+    fecha           date,
+    id_cliente      int(11),
+    id_producto     int(11),
+    cantidad        int(11),
+    valor_unitario     float(10,2),
+    CONSTRAINT      pk_factura  PRIMARY KEY(id_factura),
+    CONSTRAINT      fr_cliente   FOREIGN KEY(id_cliente) REFERENCES t_cliente(id_cliente),
+    CONSTRAINT      fr_producto   FOREIGN KEY(id_producto) REFERENCES t_producto(id_producto)
+
 
 )ENGINE=InnoDb;
